7c48020777cd8c499163d4fbafa85310a25016a2
Suldanka <39022521+sulayman101@users.noreply.github.com>
2023-08-10 07:16:46 -0700

    Update dbConn.php

--- dbConn.php
+++ dbConn.php
@@ -1,13 +1,15 @@
+<?php
 $servername = "MYSQL5038.site4now.net";
 $username = "a799ac_xallye";
 $password = "Xalliye@123";
 
 $dbName = "db_a799ac_xallye";
 //*/
-// Create connection
+// Create a connection
 $conn = new mysqli($servername, $username, $password, $dbName);
 
 // Check connection
 if (!$conn) {
   echo "Connection failed: ";
 }
+?>
